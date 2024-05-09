afafff13abf88c3504c2e5e7598ea159e0e4cc81
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2024-05-09 03:20:38 -0400

    connect model to frontend

--- frontend/src/app/CounselorAI.py
+++ frontend/src/app/CounselorAI.py
@@ -2,19 +2,39 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import speech_recognition as sr
 import requests
+from dotenv import load_dotenv
+import os
+from model.mongoRAG import OpenAI_init_LLM, rag_template, conn_to_cluster, get_vectorstore, process
 
 app = Flask(__name__)
 CORS(app)  
 
+# load environmental variables
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+MONGO_URI = os.getenv('MONGO_URI')
 
 # Initialize the recognizer
 r = sr.Recognizer()
 
+# model initialization
+llm = OpenAI_init_LLM()
+
+# llm prompt initialization
+custom_rag_prompt = rag_template()
+
+# connect to mongodb
+MONGODB_COLLECTION = conn_to_cluster(MONGO_URI)
+
+# get vectorstore
+vectorstore = get_vectorstore(MONGODB_COLLECTION)
+
 @app.route('/counselorai', methods=['POST'])
 def counselorai():
     text = request.json.get('text', '')
-    print("message: ", text)
-    response = jsonify({"text": "Therapist: " + text + " working"})
+    llm_output = process(llm, vectorstore, text)
+    response = jsonify({"text": "Therapist: " + llm_output})
     return response
 
 @app.route('/speech_to_text', methods=['POST','GET'])
--- frontend/src/app/model/mongoRAG.py
+++ frontend/src/app/model/mongoRAG.py
@@ -1,19 +1,7 @@
-# !pip install gradio icecream langchain openai sentence-transformers tiktoken langchain-mongodb langchain-openai
-# !python -m pip install "pymongo[srv]"
-
-# Import libraries.
-import getpass
-import gradio as gr
 import os
-import pprint
-import requests
-import sys
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from gradio.themes.base import Base
-from google.colab import drive
-from icecream import ic
 from langchain.callbacks.tracers import ConsoleCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -21,26 +9,16 @@ from langchain_community.llms import HuggingFacePipeline
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.document_loaders import TextLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import MongoDBAtlasVectorSearch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 #load conn to mongodb atlas search & initialize vector search?
 
-def load_env_vars():
-#Secret Key Loading , Please readdress to load as env var
-    os.environ["OPENAI_API_KEY"] = getpass.getpass()
-    os.environ["MONGO_URI"] = getpass.getpass()
-# Retrieve environment variable.
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    MONGO_URI = os.getenv('MONGO_URI')
-    
-
-def conn_to_cluster():
+def conn_to_cluster(MONGO_URI):
     mclient = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 # Connect to MongoDB Atlas cluster using the connection string.
     cluster = MongoClient(MONGO_URI)
@@ -54,15 +32,8 @@ def conn_to_cluster():
 
     vector_search_index = "vector_index"
     
-#this can be used to debug connection issues, doesnt need to be used but can be
-def ping_mdb_client():
-    try:
-# The ismaster command is cheap and does not require auth.
-        mclient.admin.command('ismaster')
-        print("MongoDB is connected")
-    except Exception as e:
-        print("Failed to connect to MongoDB", e)
-        
+    return MONGODB_COLLECTION
+
 def OpenAI_init_LLM():
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.4) # Increasing the temperature, the model becomes more creative and takes longer for inference.
     return llm
@@ -80,13 +51,15 @@ Question: {question}
 """
 #History: {history} - possible template addition to get historical context of conversation. Maybe summarizer model?
     custom_rag_prompt = ChatPromptTemplate.from_template(template)
+    return custom_rag_prompt
     
 #initialize the retriever with a vector_search as retriever by similarity, getting the k'th highest scores after .89
+'''
 def retriever_init():
     retriever = vector_search.as_retriever(
         search_type = "similarity", # similarity, mmr, similarity_score_threshold. https://api.python.langchain.com/en/latest/vectorstores/langchain_core.vectorstores.VectorStore.html#langchain_core.vectorstores.VectorStore.as_retriever
         search_kwargs = {"k": 10, "score_threshold": 0.89}
-)
+    )
     return retriever
 
 #format documents for rag_chain initializer
@@ -98,7 +71,7 @@ def rag_chain_init():
     { "context": retriever | format_docs, "question": RunnablePassthrough()}
     | custom_rag_prompt
     | llm
-    | StrOutputParser()
+    | StrOutputParser() 
 )
     
     return rag_chain
@@ -110,3 +83,38 @@ def query_answer(in_text , rag_chain):
     source_docs = retriever.get_relevant_documents(query)
     
     return answer
+'''
+def get_vectorstore(MONGODB_COLLECTION):
+    vectorstore = MongoDBAtlasVectorSearch(
+        MONGODB_COLLECTION,
+        OpenAIEmbeddings(),
+        index_name = "vector_index"
+    )
+    return vectorstore
+
+def source_info(vectorstore, prompt):
+    sourced_info = vectorstore.similarity_search(prompt, k=1)
+    return sourced_info
+
+def generate_full_prompt(sourced_info, prompt):
+    combined_information = f""" 
+    Original Input: {prompt}\n\n Answer Query With Search Results From Here: {sourced_info} . \n
+    """
+    return combined_information
+
+def get_response(client, full_prompt):
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+    {"role": "system", "content": "You are a professional Blackjack player, named Big Dog, coaching someone who doesn't know what to do with their hand."},
+    {"role": "user", "content": full_prompt}
+    ],
+    temperature = 0.3
+    )
+    return response.choices[0].message.content
+
+def process(client, vectorstore, text):
+    sourced_info = source_info(vectorstore, text)
+    full_prompt = generate_full_prompt(sourced_info, text)
+    response = get_response(client, full_prompt)
+    return response
